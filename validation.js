c40a3a7fdb9bc686ac50e8c83f7cbad3921874af
Michael Storozhilov <mrmixsun@gmail.com>
2019-03-06 00:17:07 +0700

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,3 +1,7 @@
+// Окей, раз ты добрался сюда, значит, тебе не всё равно. 
+// У нас есть отличная вакансия для фронтендера — https://hh.ru/vacancy/30248653. 
+// Приходи на Inside Out, интервью можно будет пройти на месте. 
+
 function passw(){
 var txt = document.getElementById("text").value;
 if (txt == 'test' || txt == 'promo') {
