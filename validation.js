b40c63727713955c2267e87fbfcba50247427cab
Michael Storozhilov <mrmixsun@gmail.com>
2019-03-05 15:00:40 +0700

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,16 @@
+function passw(){
+var txt = document.getElementById("text").value;
+if (txt == 'test' || txt == 'promo') {
+$('.t-prefix_1').css('display', 'block'); // Делает див видимым(по умолчанию).
+document.getElementById("text").style.display="none";
+document.getElementById("buttonform").style.display="none";
+document.getElementById("error").style.display="none";
+document.getElementById("succes").style.display="block";
+document.getElementById("rec90671125").style.display="block";
+document.getElementById("rec91182509").style.display="none";
+}
+else { 
+    document.getElementById("error").style.display="block";
+	}
+}
+
