bafd5a6905da9ee92f5bc443a67b0c9ede94f01e
Michael Storozhilov <mrmixsun@gmail.com>
2019-03-06 19:44:51 +0700

    Add files via upload

--- validation.js
+++ validation.js
@@ -4,7 +4,31 @@
 
 function passw(){
 var txt = document.getElementById("text").value;
-if (txt == 'test' || txt == 'promo') {
+if (txt == 'J8xIG5' || txt == 'DxOVav' || txt == 'C8GVqS' || txt == 's1jJkX' || txt == 'c5TBLn' || txt == 'BpcOjQ' || txt == 'E4xAJC' || txt == '8dZS9x' 
+	|| txt == 'BaaT1K' || txt == 'NsDr0y' || txt == 'KBTRyc' || txt == 'LWp2Lc' || txt == 'MTHC5b' || txt == 'buBvvv' || txt == '93PefN' || txt == 'GFX195'
+	|| txt == 'Rogbz6' || txt == 'Ojiw3u' || txt == 'SHUvZ7' || txt == 'TloJDo' || txt == 'vt61iw' || txt == 'sa0JNz' || txt == 'DiMz0K' || txt == 'HSiC82'  
+	|| txt == 'y9Z6lL' || txt == 'F8rxZq' || txt == 'ddeSrx' || txt == '84nFGz' || txt == 'QWHBnE' || txt == '97J9fr' || txt == 'FTjtcJ' || txt == 'ptW0HJ'
+	|| txt == 'AXWrJx' || txt == 'crkHGW' || txt == 'j1fOEU' || txt == '30QkDG' || txt == 'qDJjaY' || txt == 'zxMxGj' || txt == 't0PDHN' || txt == 'G6G2RJ'  
+	|| txt == 'hxJm8y' || txt == 'FSqeEt' || txt == 'zWq00T' || txt == 'eFPOk5' || txt == 'MJNwHg' || txt == 'YvY5n2' || txt == 'MvESOh' || txt == 'FO0SiL' 
+	|| txt == 'k6N8xC' || txt == 'r9i4wa' || txt == 'NAsnAs' || txt == '4dnEcy' || txt == '8SvVdE' || txt == 'QX8MLl' || txt == 'cckLhO' || txt == '3RLt3e' 
+	|| txt == '4ZRssz' || txt == 'ztbE9j' || txt == 'yTes9K' || txt == 'eBRtpS' || txt == 'PbXP03' || txt == 'F7Hsmc' || txt == 'bUltn1' || txt == 'ehzktI' 
+	|| txt == 'w3Sytv' || txt == 'GFKEJH' || txt == 'lcZAVK' || txt == 'wnhyZH' || txt == 'jrEWdW' || txt == '4T5TpN' || txt == '2ndO13' || txt == 'NpglzQ' 
+	|| txt == '0SmhkO' || txt == '5Ns8MG' || txt == '9LZ4B4' || txt == 'LHk6Bo' || txt == 'bvIwt2' || txt == 'etEKPx' || txt == 'qTOEWi' || txt == 'JQ1a4F'
+	|| txt == 'rDeAI1' || txt == 'z6f1BI' || txt == 'lmscIc' || txt == 'yimfp1' || txt == 'A12Qqc' || txt == 'G58kYY' || txt == 'onWShN' || txt == 'LBG5OC' 
+	|| txt == 'jf4yJr' || txt == 'VwXFwx' || txt == '9jh7Bx' || txt == 'E6oRIF' || txt == 'wqsjCz' || txt == 'WWV6uf' || txt == 'N4TlIu' || txt == 'rUDOEa'
+	|| txt == '3TuwOf' || txt == 'hpTJHM' || txt == '0KqVD8' || txt == 'Qs1TX9' || txt == 'snTn8I' || txt == 'N1D6KX' || txt == 'o74fFP' || txt == 'EFqepf' 
+	|| txt == 'MaHJha' || txt == 'GJNLTV' || txt == 'qrDdNt' || txt == '78w1ag' || txt == '0d2qYw' || txt == 'TtWZcj' || txt == '5Q7LZW' || txt == 'gzt2C4'
+	|| txt == 'CK1vIw' || txt == 'qIRLiO' || txt == 'zsfc6M' || txt == 'emMMOl' || txt == 'qbw6gv' || txt == 'n0ThvD' || txt == 'UFZAy1' || txt == 'ZHw8QB' 
+	|| txt == 'VeDc2X' || txt == 'rLO6Fv' || txt == 'lEvvlo' || txt == '9ncK0K' || txt == 'OLuaJi' || txt == '8dQAN2' || txt == 'y2AfbN' || txt == 'MyqSKO'
+	|| txt == 'xJ8uo8' || txt == 'bjJZJu' || txt == '1FO1Nu' || txt == 'l4N4uS' || txt == 'K7jlO9' || txt == '13GnNv' || txt == 'cSrWyz' || txt == 'oV8jFC' 
+	|| txt == 'tHZ8Km' || txt == 'kmSguN' || txt == 'QzXHCP' || txt == 'WOJzdT' || txt == 'pYxLbG' || txt == 'qKtxGc' || txt == 'p6scaO' || txt == 'Aa3ymF' 
+	|| txt == 'h1V6a0' || txt == 'QPTxe2' || txt == '9Oy2fC' || txt == 'TLDqGv' || txt == 'qG6t9N' || txt == 'V8JyM5' || txt == '4gIFtU' || txt == 'OrHfX3'
+	|| txt == '9MSA1q' || txt == '7Jg3xd' || txt == 'JIaid0' || txt == 'wyfpzO' || txt == 'h2s5t7' || txt == 'brC0fS' || txt == 'MawReD' || txt == 'jlZJJ7'
+	|| txt == 'Jle5KS' || txt == 'E8Eg3i' || txt == 'ahQ7hg' || txt == 'ITGpOj' || txt == 'vmMUUh' || txt == 'EVNtJh' || txt == 'hZfl8y' || txt == 'JoAJtm'
+	|| txt == '5d5OSE' || txt == 'm93GT9' || txt == '5UXLas' || txt == '9J5s93' || txt == 'UGwKy6' || txt == 'IMiEoz' || txt == 'TNl7fv' || txt == '5i3H1r'
+	|| txt == '2mzvwk' || txt == 'go7Y1w' || txt == 'W82p0M' || txt == 'ZTbWWw' || txt == 'b70jDm' || txt == 'PRh3mI' || txt == 'pthuay' || txt == '8srnSe'
+	|| txt == 'GtiEyp' || txt == 'rZye0k' || txt == 'LOc9Wa' || txt == 'YsTxIg' || txt == 'JTJz4w' || txt == 'DI6zAw' || txt == 'ZfcnUx' || txt == 'uN4KV5'
+	|| txt == '3ey7Dd' || txt == 'ark1Da' || txt == 'LNlFO3' || txt == 'hwuqJd' || txt == 'SbWTTN' || txt == '081y4R' || txt == 'OtumCz' || txt == 'qfIO2J') {
 $('.t-prefix_3').css('display', 'block'); // Делает див видимым(по умолчанию).
 document.getElementById("text").style.display="none";
 document.getElementById("buttonform").style.display="none";
